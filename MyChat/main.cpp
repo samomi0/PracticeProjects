856bff50575cfaab1ed3e5eefa00f3e5b1f15b22
samomi0 <love162@163.com>
2021-11-13 11:27:16 +0800

    commit

---
+++ MyChat/main.cpp
@@ -0,0 +1,11 @@
+#include "chatserverdialog.h"
+
+#include <QApplication>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    ChatServerDialog w;
+    w.show();
+    return a.exec();
+}
